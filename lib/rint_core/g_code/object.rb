a8b83785df0738ca5874c1b0ef17b5198b5ec616
Kaz Walker <kaz.walker@slipline.ca>
2013-02-25 17:22:14 -0700

    [GCode::Object] Refactored measurement function to simplify it.

--- lib/rint_core/g_code/object.rb
+++ lib/rint_core/g_code/object.rb
@@ -46,91 +46,6 @@ private
         end
       end
 
-      def measure
-        x_min = 999999999
-        y_min = 999999999
-        z_min = 0
-        x_max = -999999999
-        y_max = -999999999
-        z_max = -999999999
-
-        current_x = 0
-        current_y = 0
-        current_z = 0
-
-        @x_travel = 0
-        @y_travel = 0
-        @z_travel = 0
-
-        @lines.each do |line|
-          if line.command == SET_POSITION
-            current_x = line.x if line.x.present?
-            current_y = line.y if line.y.present?
-            current_z = line.z if line.z.present?
-          elsif line.command == HOME
-            if line.x.present? || line.full_home?
-              @x_travel += current_x
-              current_x = 0
-            end
-            if line.y.present? || line.full_home?
-              @y_travel += current_y
-              current_y = 0
-            end
-            if line.z.present? || line.full_home?
-              @z_travel += current_z
-              current_z = 0
-            end
-          elsif line.is_move?
-            x = line.x
-            y = line.y
-            z = line.z
-            
-            if line.relative
-              @x_travel += x.abs if x.present?
-              @y_travel += y.abs if y.present?
-              @z_travel += z.abs if z.present?
-              x.present? ? x = current_x + x : x = current_x
-              y.present? ? y = current_y + y : y = current_y
-              z.present? ? z = current_z + z : z = current_z
-            else
-              @x_travel += (current_x - x).abs if x.present?
-              @y_travel += (current_y - y).abs if y.present?
-              @z_travel += (current_z - z).abs if z.present?
-            end
-
-            if x.present? && !x.zero? && line.extrusion_move?
-              x_min = x if x < x_min
-              x_max = x if x > x_max
-            end
-
-            if y.present? && !y.zero? && line.extrusion_move?
-              y_min = y if y < y_min
-              y_max = y if y > y_max
-            end
-
-            if z.present?
-              z_min = z if z < z_min
-              z_max = z if z > z_max
-            end
-
-            current_x = (x > current_x ? x : current_x) if x.present?
-            current_y = (y > current_y ? y : current_y) if y.present?
-            current_z = (z > current_z ? z : current_z) if z.present?
-          end
-        end
-
-        @x_min = x_min
-        @x_max = x_max
-        @y_min = y_min
-        @y_max = y_max
-        @z_min = z_min
-        @z_max = z_max
-
-        @width = x_max - x_min
-        @depth = y_max - y_min
-        @height = z_max - z_min
-      end
-
       def calculate_filament_usage
         return @filament_used if @filament_used.present?
         current_e = 0
@@ -147,6 +62,99 @@ private
         end
 
         @filament_used = total_e
+      end
+
+      def measure
+        set_variables
+        @lines.each do |line|
+          if line.command == SET_POSITION
+            @current_x = line.x if line.x.present?
+            @current_y = line.y if line.y.present?
+            @current_z = line.z if line.z.present?
+          elsif line.command == HOME
+            reset_axes(line)
+          elsif line.is_move?
+            measure_travel(line)
+            set_current_position(line)
+            set_limits(line)
+          end
+        end
+
+        @width = x_max - x_min
+        @depth = y_max - y_min
+        @height = z_max - z_min
+      end
+
+      def measure_travel(line)
+        if line.relative
+          @x_travel += line.x.abs if line.x.present?
+          @y_travel += line.y.abs if line.y.present?
+          @z_travel += line.z.abs if line.z.present?
+        else
+          @x_travel += (@current_x - line.x).abs if line.x.present?
+          @y_travel += (@current_y - line.y).abs if line.y.present?
+          @z_travel += (@current_z - line.z).abs if line.z.present?
+        end
+      end
+
+      def reset_axes(line)
+        if line.x.present? || line.full_home?
+          @x_travel += @current_x
+          @current_x = 0
+        end
+        if line.y.present? || line.full_home?
+          @y_travel += @current_y
+          @current_y = 0
+        end
+        if line.z.present? || line.full_home?
+          @z_travel += @current_z
+          @current_z = 0
+        end
+      end
+
+      def set_current_position(line)
+        if line.relative
+          @current_x += line.x if line.x.present?
+          @current_y += line.y if line.y.present?
+          @current_z += line.z if line.z.present?
+        else
+          @current_x = line.x if line.x.present?
+          @current_y = line.y if line.y.present?
+          @current_z = line.z if line.z.present?
+        end
+      end
+
+      def set_limits(line)
+        if line.extrusion_move?
+          if line.x.present? && !line.x.zero?
+            @x_min = @current_x if @current_x < @x_min
+            @x_max = @current_x if @current_x > @x_max
+          end
+          if line.y.present? && !line.y.zero?
+            @y_min = @current_y if @current_y < @y_min
+            @y_max = @current_y if @current_y > @y_max
+          end
+        end
+        if line.z.present?
+          @z_min = @current_z if @current_z < @z_min
+          @z_max = @current_z if @current_z > @z_max
+        end
+      end
+
+      def set_variables
+        @x_travel = 0
+        @y_travel = 0
+        @z_travel = 0
+        @current_x = 0
+        @current_y = 0
+        @current_z = 0
+        @current_e = 0
+        @x_min = 999999999
+        @y_min = 999999999
+        @z_min = 0
+        @x_max = -999999999
+        @y_max = -999999999
+        @z_max = -999999999
       end
 
     end
