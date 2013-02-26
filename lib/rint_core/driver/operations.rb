1d1402ea52000f3ebce82b015c1e185e66d9b335
Kaz Walker <kaz.walker@slipline.ca>
2013-02-26 11:40:10 -0700

    [Driver::Operations] Made early restart catching more conservative.

--- lib/rint_core/driver/operations.rb
+++ lib/rint_core/driver/operations.rb
@@ -140,13 +140,10 @@ private
             clear_to_send!
           when :online
             config.callbacks[:receive].call(line) if config.callbacks[:receive].present?
-            if printing? && @queue_index < 10
+            if printing? && @queue_index.zero?
               sleep(config.long_sleep)
               clear_to_send!
-              @queue_index = 0
             end
-          when :echo
-            config.callbacks[:echo].call(line) if config.callbacks[:echo].present?
           when :temperature
             config.callbacks[:temperature].call(line) if config.callbacks[:temperature].present?
           when :temperature_response
