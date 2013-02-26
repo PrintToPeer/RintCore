991616f3aaafab12271174f786a76b9d5fb75ae4
Kaz Walker <kaz.walker@slipline.ca>
2013-02-26 01:10:48 -0700

    [RintCore::Cli] Reduce # of puts calls.

--- lib/rint_core/cli.rb
+++ lib/rint_core/cli.rb
@@ -10,17 +10,17 @@ module RintCore
     def analyze(file)
       if File.exist?(file) && File.file?(file)
         object = RintCore::GCode::Object.new(IO.readlines(file))
-        puts "Dimensions:"
-        puts "\tX: #{object.x_min.round(options[:decimals])} - #{object.x_max.round(options[:decimals])} (#{object.width.round(options[:decimals])}mm)"
-        puts "\tY: #{object.y_min.round(options[:decimals])} - #{object.y_max.round(options[:decimals])} (#{object.depth.round(options[:decimals])}mm)"
-        puts "\tZ: #{object.z_min.round(options[:decimals])} - #{object.z_max.round(options[:decimals])} (#{object.height.round(options[:decimals])}mm)"
-        puts "Total Travel:"
-        puts "\tX: #{object.x_travel.round(options[:decimals])}mm"
-        puts "\tY: #{object.y_travel.round(options[:decimals])}mm"
-        puts "\tZ: #{object.z_travel.round(options[:decimals])}mm"
-        puts "Filament used: #{object.filament_used.round(options[:decimals])}mm"
-        puts "Number of layers: #{object.layers}"
-        puts "#{object.raw_data.length} lines / #{object.lines.length} commands"
+        puts "Dimensions:
+\tX: #{object.x_min.round(options[:decimals])} - #{object.x_max.round(options[:decimals])} (#{object.width.round(options[:decimals])}mm)
+\tY: #{object.y_min.round(options[:decimals])} - #{object.y_max.round(options[:decimals])} (#{object.depth.round(options[:decimals])}mm)
+\tZ: #{object.z_min.round(options[:decimals])} - #{object.z_max.round(options[:decimals])} (#{object.height.round(options[:decimals])}mm)
+Total Travel:
+\tX: #{object.x_travel.round(options[:decimals])}mm
+\tY: #{object.y_travel.round(options[:decimals])}mm
+\tZ: #{object.z_travel.round(options[:decimals])}mm
+Filament used: #{object.filament_used.round(options[:decimals])}mm
+Number of layers: #{object.layers}
+#{object.raw_data.length} lines / #{object.lines.length} commands"
       else
         puts "Non-exsitant file: #{file}"
       end
